dab6ce6d94cb6cebaeb800d2e6d018765141d3d3
N-eeraj <neeraj7rajeena@gmail.com>
2022-04-09 01:12:35 +0530

    changed prompt to custom modal

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const app = Vue.createApp({
                 itemCount: 0,
                 totalPrice: 0
             },
+            prompt: false,
+            selectedItemCount: 0,
+            selectedItemIndex: null,
             loading: true
         }
     },
@@ -19,12 +22,30 @@ const app = Vue.createApp({
             })
     },
     methods: {
-        calculateCartTotal() {
-            let cart = this.cart
-            let total = 0
-            cart.items.forEach(item => total += item.price)
-            cart.totalPrice = total
-            console.log(cart)
+        selectItem(index) {
+            this.prompt = true
+            this.selectedItemIndex = index
+        },
+        getItemCount() {
+            let num = this.selectedItemCount
+            let stock = this.inventory[this.selectedItemIndex].stock
+            if (isNaN(num))
+                return alert("Enter a Number")
+            if (num < 1 || !Number.isInteger(num))
+                return alert("Invalid Number")
+            if (num > stock)
+                return alert(`Only ${stock} items in stock`)
+            this.prompt = false
+            this.updateInventory(num)
+        },
+        updateInventory(itemCount) {
+            let index = this.selectedItemIndex
+            let item = this.inventory[index]
+            item.stock -= itemCount
+            this.selectedItemCount = 0
+            console.log(this.inventory)
+            this.addToCart(item.name, itemCount, item.price * itemCount)
+            this.calculateCartTotal()
         },
         addToCart(name, count, price) {
             let cartItems = this.cart.items
@@ -43,44 +64,41 @@ const app = Vue.createApp({
             })
             ++this.cart.itemCount
         },
-        updateInventory(index, itemCount) {
-            let item = this.inventory[index]
-            item.stock -= itemCount
-            console.log(this.inventory)
-            this.addToCart(item.name, itemCount, item.price * itemCount)
-            this.calculateCartTotal()
+        calculateCartTotal() {
+            let cart = this.cart
+            let total = 0
+            cart.items.forEach(item => total += item.price)
+            cart.totalPrice = total
+            console.log(cart)
         }
     },
     template: `
         <h1>Fruit Basket</h1>
         <div id="fruit_list">
-            <fruit v-for="(fruit, index) in inventory" :fruit="fruit" :index="index" @stockChange="updateInventory" :key="index" />
+            <fruit v-for="(fruit, index) in inventory" :fruit="fruit" :index="index" @openModal="selectItem" @stockChange="updateInventory" :key="index" />
         </div>
         <button class="cart-toggle" :data-count="cart.itemCount"><i class="fa-solid fa-basket-shopping"></i></button>
+        <div class="overlay" v-show="prompt">
+            <div class="modal">
+                <input type="number" placeholder="Enter Count" v-model="selectedItemCount" focus />
+                <div>
+                    <button @click="getItemCount">Continue</button>
+                    <button @click="prompt=false">Cancel</button>
+                </div>
+            </div>
+        </div>
         <div class="loader" v-if="loading"></div>
     `
 })
 app.component("fruit", {
     props: ["fruit", "index"],
-    methods: {
-        addToCart(index, stock) {
-            let num = Number(prompt("Enter Number of items"))
-            if (isNaN(num))
-                return alert("Enter a Number")
-            if (num < 1 || !Number.isInteger(num))
-                return alert("Invalid Number")
-            if (num > stock)
-                return alert(`Only ${stock} items in stock`)
-            this.$emit("stockChange", index, num)
-        }
-    },
     template: `
         <div class="fruit-item">
             <img :src="fruit.image" />
             <div class="item-details">
                 <span>{{fruit.name}}</span>
                 <span>₹{{fruit.price}}/Kg</span>
-                <button @click="addToCart(index, fruit.stock)">Add to Cart</button>
+                <button @click="() => this.$emit('openModal', index)" >Add to Cart</button>
             </div>
         </div>
     `
